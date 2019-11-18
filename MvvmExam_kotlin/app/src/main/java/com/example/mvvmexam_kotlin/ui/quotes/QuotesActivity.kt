2494c6a60da4fdf71b27a930e1fc2e2b455272f2
seunghyon321 <seunghyon321@gmail.com>
2019-11-19 00:09:32 +0900

    MVVM Exam - Kotlin

--- MvvmExam_kotlin/app/src/main/java/com/example/mvvmexam_kotlin/ui/quotes/QuotesActivity.kt
+++ MvvmExam_kotlin/app/src/main/java/com/example/mvvmexam_kotlin/ui/quotes/QuotesActivity.kt
@@ -2,12 +2,45 @@ package com.example.mvvmexam_kotlin.ui.quotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.example.mvvmexam_kotlin.R
+import com.example.mvvmexam_kotlin.data.Quote
+import com.example.mvvmexam_kotlin.utilities.InjectorUtils
+import kotlinx.android.synthetic.main.activity_quotes.*
 
 class QuotesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quotes)
+        initializeUi()
+    }
+
+    private fun initializeUi() {
+        // Get the QuotesViewModelFactory with all of it's dependencies constructed
+        val factory = InjectorUtils.provideQuotesViewModelFactory()
+        // Use ViewModelProviders class to create / get already created QuotesViewModel
+        // for this view (activity)
+        val viewModel = ViewModelProviders.of(this, factory)
+            .get(QuotesViewModel::class.java)
+
+        // Observing LiveData from the QuotesViewModel which in turn observes
+        // LiveData from the repository, which observes LiveData from the DAO ☺
+        viewModel.getQuotes().observe(this, Observer { quotes ->
+            val stringBuilder = StringBuilder()
+            quotes.forEach { quote ->
+                stringBuilder.append("$quote\n\n")
+            }
+            textView_quotes.text = stringBuilder.toString()
+        })
+
+        // When button is clicked, instantiate a Quote and add it to DB through the ViewModel
+        button_add_quote.setOnClickListener {
+            val quote = Quote(editText_quote.text.toString(), editText_author.text.toString())
+            viewModel.addQuotes(quote)
+            editText_quote.setText("")
+            editText_author.setText("")
+        }
     }
 }
