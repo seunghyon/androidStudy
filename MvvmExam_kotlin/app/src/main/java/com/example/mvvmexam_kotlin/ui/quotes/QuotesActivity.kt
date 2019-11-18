005758b4c4e43e1f8db16d7ade9fe24d8a2ab664
seunghyon321 <seunghyon321@gmail.com>
2019-11-18 23:51:58 +0900

    MVVM Exam - Kotlin

---
+++ MvvmExam_kotlin/app/src/main/java/com/example/mvvmexam_kotlin/ui/quotes/QuotesActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mvvmexam_kotlin.ui.quotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mvvmexam_kotlin.R
+
+class QuotesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quotes)
+    }
+}
