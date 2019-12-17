f19ce9a36b25e20bfa0dd5404f021c7710a5e9ce
seunghyon321 <seunghyon321@gmail.com>
2019-12-17 19:39:59 +0900

    android exam

--- MvvmExam_kotlin/app/src/main/java/com/example/mvvmexam_kotlin/data/Quote.kt
+++
@@ -1,8 +0,0 @@
-package com.example.mvvmexam_kotlin.data
-
-data class Quote(val quoteText: String,
-                 val author: String) {
-    override fun toString(): String {
-        return "$quoteText - $author"
-    }
-}
