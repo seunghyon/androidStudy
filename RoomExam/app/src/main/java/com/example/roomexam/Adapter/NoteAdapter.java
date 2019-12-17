186df5d1ac03efc8ce5e383ddf99842146dd67a0
seunghyon321 <seunghyon321@gmail.com>
2019-12-17 19:41:45 +0900

    android exam

---
+++ RoomExam/app/src/main/java/com/example/roomexam/Adapter/NoteAdapter.java
@@ -0,0 +1,111 @@
+package com.example.roomexam.Adapter;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.roomexam.R;
+import com.example.roomexam.data.Note;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteAdapter extends ListAdapter<Note,NoteAdapter.NoteHolder> {
+
+    //private List<Note> notes = new ArrayList<>();
+    private OnItemClickListener listener;
+
+    public NoteAdapter() {
+        super(DIFF_CALLBACK);
+    }
+
+    private static final DiffUtil.ItemCallback<Note> DIFF_CALLBACK = new DiffUtil.ItemCallback<Note>() {
+        @Override
+        public boolean areItemsTheSame(@NonNull Note oldItem, @NonNull Note newItem) {
+            return oldItem.getId() == newItem.getId();
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull Note oldItem, @NonNull Note newItem) {
+            return oldItem.getTitle().equals(newItem.getTitle()) &&
+                    oldItem.getDescription().equals(newItem.getDescription()) &&
+                    oldItem.getPriority()==newItem.getPriority();
+        }
+    };
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.note_item, parent, false);
+
+        return new NoteHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder holder, int position) {
+
+        //Note note = notes.get(position);
+        Note note = getItem(position);
+
+        holder.textViewTitle.setText(note.getTitle());
+        holder.textViewDescription.setText(note.getDescription());
+        holder.textViewPriority.setText(String.valueOf(note.getPriority()));
+
+        Log.e("onBindViewHolder ", note.getDescription() + " " + position);
+    }
+/*
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public void setNotes(List<Note> notes) {
+        this.notes = notes;
+        notifyDataSetChanged();
+    }
+*/
+    public Note getNoteAt(int position) {
+        //return notes.get(position);
+        return getItem(position);
+    }
+
+    class NoteHolder extends RecyclerView.ViewHolder {
+        private TextView textViewTitle;
+        private TextView textViewDescription;
+        private TextView textViewPriority;
+
+        public NoteHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewTitle = itemView.findViewById(R.id.text_view_title);
+            textViewDescription = itemView.findViewById(R.id.text_view_description);
+            textViewPriority = itemView.findViewById(R.id.text_view_priority);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    if (listener != null && position != RecyclerView.NO_POSITION) {
+                        //listener.onItemClick(notes.get(position));
+                        listener.onItemClick(getItem(position));
+                    }
+                }
+            });
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(Note note);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
+}
---
+++ RoomExam/app/src/main/java/com/example/roomexam/viewmodel/NoteViewModel.java
@@ -0,0 +1,45 @@
+package com.example.roomexam.viewmodel;
+
+import android.app.Application;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.roomexam.data.Note;
+import com.example.roomexam.data.NoteRepository;
+
+import java.util.List;
+
+public class NoteViewModel extends AndroidViewModel {
+
+    private NoteRepository repository;
+    private LiveData<List<Note>> allNotes;
+
+    public NoteViewModel(@NonNull Application application) {
+        super(application);
+        repository = new NoteRepository(application);
+        allNotes = repository.getAllNotes();
+    }
+
+    public void insert(Note note){
+        repository.insert(note);
+    }
+
+    public void update(Note note){
+        repository.update(note);
+    }
+
+    public void delete(Note note){
+        repository.delete(note);
+    }
+
+    public void deleteAllNotes(){
+        repository.deleteAllNotes();
+    }
+
+    public LiveData<List<Note>> getAllNotes(){
+        return allNotes;
+    }
+}
